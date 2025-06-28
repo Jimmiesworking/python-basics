cbb40e5d65c38fd5c3ef2c25471ebfc9e9c30bff
JimmyCatrin <jimmiesworking@gmail.com>
2025-06-28 21:46:47 +0300

    Добавил остальные примеры функции

--- functions_examples.py
+++ functions_examples.py
@@ -6,3 +6,15 @@ def greet(name):
 	return f"Привет, {name}"
 
 print(greet("Мир"))
+
+# Пример 2: Сумма двух чисел
+
+def sum_numbers(a, b):
+        return a + b
+
+print(sum_numbers(4, 7)) #11
+
+# Пример 3: Умножение двух чисел
+
+def multiply(a, b):
+        return a * b
